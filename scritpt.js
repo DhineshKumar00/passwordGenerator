bf082270cce35343956c09b95c9418c85624a57c
Dhinesh_Kumar_M <140738063+DhineshKumar00@users.noreply.github.com>
2024-12-18 12:16:02 +0530

    Add files via upload

---
+++ scritpt.js
@@ -0,0 +1,35 @@
+const passwordBox=document.getElementById("password");
+
+const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase="abcdefghijklmnopqrstuvwxyz";
+const number="0123456789";
+const symbol="~!@#$%^&*()-_+=?></|[]{} ";
+const allChars = upperCase+lowerCase+number+symbol;
+
+// Creating Password for given length;
+function createPassword(){
+    const pwdlength = document.getElementById("passwordlength").value;
+    let password="";
+    password+=upperCase[Math.floor(Math.random() * upperCase.length)];
+    password+=lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password+=number[Math.floor(Math.random() * number.length)];
+    password+=symbol[Math.floor(Math.random() * symbol.length)];
+    while(pwdlength > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }   
+    passwordBox.value=password;
+}
+
+//Copy input element
+function copyElement(){
+    var copyText=document.getElementById("password");
+    if(copyText.value==""){
+        alert("please click Generate password before copy");
+    }else{
+    copyText.hidden=false;
+    copyText.select();
+    document.execCommand("copy");
+    copyText.text=true;
+    alert("copied text is: "+copyText.value);
+    }
+}
